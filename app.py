857db4f337fdf741502bac6ee0f9be19939c2140
mlbtheroku <115041575+mlbtheroku@users.noreply.github.com>
2023-07-27 03:45:22 +0630

    Deployable Webservice

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return """
+<center> 
+    <img src="https://giphy.com/gifs/GitHub-ok-thumbs-up-thumb-NytMLKyiaIh6VH9SPm/giphy.webp" style="border-radius: 12px;"/> 
+</center> 
+<style>
+    body { 
+        background: antiquewhite;
+    }
+</style>"""
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
